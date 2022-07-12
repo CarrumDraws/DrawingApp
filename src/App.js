36b6ce3e9052345d16a1f227ba84530c143f1ad1
CarrumDraws <calumchansy@gmail.com>
2022-07-11 22:23:50 -0700

    New Branch Data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useLayoutEffect, useState } from "react";
-import { RoughCanvas } from "roughjs/bin/canvas";
 import rough from "roughjs/bundled/rough.esm.js";
 
-const generator = rough.generator(); // Declare Generator
+const generator = rough.generator(); // Generator used for shape generation
 
-// Creates Line
+// Creates Shapes
 function createElement(x1, y1, x2, y2) {
   const roughElement = generator.line(x1, y1, x2, y2);
   return { x1, y1, x2, y2, roughElement };
@@ -18,34 +17,34 @@ function App() {
   // Use this if your DOM's appearance changes between its rendering and its mutation.
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d"); // The context is what we use to render our drawings on to.
+    const context = canvas.getContext("2d");
     context.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-    const roughCanvas = rough.canvas(canvas); // Declare a Rough Canvas
+    const roughCanvas = rough.canvas(canvas); // Declare a rough.canvas
 
-    //Create shapes with generator
-    // const line = generator.line(80, 120, 300, 100); // x1, y1, x2, y2
-    // roughCanvas.draw(line);
-    elements.forEach(({ roughElement }) => roughCanvas.draw(roughElement)); // Desturcture the roughElement out of every element in elements
+    // rough.canvas.draw appends displays the shape onto the DOM
+    // Desturcture 'roughElement' shape out of 'elements' state, draw the shape
+    elements.forEach(({ roughElement }) => roughCanvas.draw(roughElement));
   }, [elements]);
 
   const handleMouseDown = (event) => {
     setDrawing(true);
-    const { clientX, clientY } = event;
+    const { clientX, clientY } = event; // Starting Point
     const element = createElement(clientX, clientY, clientX, clientY);
     setElements((prevState) => [...prevState, element]);
   };
-  const handleMouseMove = (event) => {
-    if (!drawing) return;
 
-    const { clientX, clientY } = event; // clientX and clientY are relative to the element, NOT the screen
+  const handleMouseMove = (event) => {
+    if (!drawing) return; // Ignore normal mouse movement
+
+    const { clientX, clientY } = event; // Ending Point
     const index = elements.length - 1;
 
-    const { x1, y1 } = elements[index]; // get the x1 and y1 of the last element- the element created in handleMouseDown...
-    const updatedElement = createElement(x1, y1, clientX, clientY); // ...and make a new element with that data.
+    const { x1, y1 } = elements[index]; // get the starting point of the last element...
+    const updatedElement = createElement(x1, y1, clientX, clientY); // ...and update its end point
 
-    const elementsCopy = [...elements]; // Replace the las element with the one yu just created.
-    elementsCopy[index] = updatedElement;
+    const elementsCopy = [...elements];
+    elementsCopy[index] = updatedElement; // Replace the last element.
     setElements(elementsCopy);
   };
   const handleMouseUp = () => {
